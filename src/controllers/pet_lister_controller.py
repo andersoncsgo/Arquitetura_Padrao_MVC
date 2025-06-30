ee5136cae63ea45fc256da5c0e6b50422d5d732a
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2025-06-30 12:18:02 -0300

    feat: Implementing pet_deleter_controller with tests

--- src/controllers/pet_lister_controller.py
+++ src/controllers/pet_lister_controller.py
@@ -3,7 +3,7 @@ from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 from src.models.sqlite.entities.pets import PetsTable
 
 class PetListerController:
-    def __init__(self, pet_repository: PetsRepositoryInterface):
+    def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
         self.__pet_repository = pet_repository
 
     def list(self) -> dict:
