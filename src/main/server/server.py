5a61eb2538bebcef3176ab0f093252aa35af04cd
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2025-07-17 17:39:40 -0300

    feat: Implementing projec routes for pets and people

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -4,9 +4,11 @@ from src.models.sqlite.settings.connection import db_connection_handler
 
 # Importar Blueprints
 from src.main.routes.pets_routes import pet_route_bp
+from src.main.routes.person_routes import person_route_bp
 
 db_connection_handler.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(pet_route_bp)
+app.register_blueprint(person_route_bp)
